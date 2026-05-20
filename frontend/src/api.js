1215cf79b9d399b69a2293d369da107cc2721d6d
Harsh Singh <harshsingh87777@gmail.com>
2026-05-21 00:17:47 +0530

    update to render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://team-task-manager-production-bf6a.up.railway.app/api',
+  baseURL: 'https://team-task-manager-f1wc.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
